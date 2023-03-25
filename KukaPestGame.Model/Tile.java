8c66ab0521cdffbfc65d380662264717193afe3c
Dienes Nikolett <zmncyq@inf.elte.hu>
2023-03-25 19:40:10 +0000

    Upload New File

---
+++ KukaPestGame.Model/Tile.java
@@ -0,0 +1,25 @@
+public class Tile {
+    private Coordinates coordinates;
+    private boolean hasElectricity;
+
+    public Tile(Coordinates coordinates, boolean hasElectricity) {
+        this.coordinates = coordinates;
+        this.hasElectricity = hasElectricity;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public boolean getHasElectricity() {
+        return hasElectricity;
+    }
+
+    public void setHasElectricity(boolean hasElectricity) {
+        this.hasElectricity = hasElectricity;
+    }
+}
