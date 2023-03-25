6abb100e9caedbe716c6b907bc785f4fcca872c7
Dienes Nikolett <zmncyq@inf.elte.hu>
2023-03-25 19:42:08 +0000

    Upload New File

---
+++ KukaPestGame.Model/Road.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Road extends Constructable{
+
+    public Road(Coordinates coordinates, boolean hasElectricity, Image currentImg, int cost, int timeToBuild) {
+        super(coordinates, hasElectricity, currentImg, cost, timeToBuild);
+    }
+}
