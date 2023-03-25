b820ade7e311360be07d877ed95db3c14dfa194b
Dienes Nikolett <zmncyq@inf.elte.hu>
2023-03-25 19:41:51 +0000

    Upload New File

---
+++ KukaPestGame.Model/Pole.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Pole extends Constructable{
+
+    public Pole(Coordinates coordinates, boolean hasElectricity, Image currentImg, int cost, int timeToBuild) {
+        super(coordinates, hasElectricity, currentImg, cost, timeToBuild);
+    }
+}
