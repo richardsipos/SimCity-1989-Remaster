adbe87a07c7438ffa3a8bf746a0a05d092ad1d8a
Dienes Nikolett <zmncyq@inf.elte.hu>
2023-03-25 19:40:28 +0000

    Upload New File

---
+++ KukaPestGame.Model/MapItem.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class MapItem extends Tile{
+
+    private Image currentImg;
+
+    public MapItem(Coordinates coordinates, boolean hasElectricity, Image currentImg) {
+        super(coordinates, hasElectricity);
+        this.currentImg = currentImg;
+    }
+
+    public Image getCurrentImg() {
+        return currentImg;
+    }
+
+    public void setCurrentImg(Image currentImg) {
+        this.currentImg = currentImg;
+    }
+}
