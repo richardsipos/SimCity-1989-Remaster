a6f3f84c0d3984a1f8054a4644862fc177742fa3
Dienes Nikolett <zmncyq@inf.elte.hu>
2023-03-25 19:40:52 +0000

    Upload New File

---
+++ KukaPestGame.Model/Environment.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Environment extends MapItem{
+
+        public Environment(Coordinates coordinates, boolean hasElectricity, Image currentImg) {
+            super(coordinates, hasElectricity, currentImg);
+    }
+}
