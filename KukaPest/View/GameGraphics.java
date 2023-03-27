dbb467d8d967da2a551de13b9dffc41ea12e9c01
Szegedi Bence Sándor <yhibpg@inf.elte.hu>
2023-03-27 10:42:56 +0200

    Push test

---
+++ KukaPest/View/GameGraphics.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.TimerTask;
+
+public class GameGraphics extends JPanel {
+
+}
--- KukaPest/View/MainWindow.java
+++ KukaPest/View/MainWindow.java
@@ -1,4 +1,31 @@
 package View;
 
+import Model.Game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TimerTask;
+
 public class MainWindow {
+    public JFrame f;
+    GameGraphics g;
+    f = new JFrame();
+    g = new GameGraphics();
+
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menu = new JMenu("Top 10");
+    JMenu ng = new JMenu("New Game");
+
+    menuBar.add(menu);
+    menuBar.add(ng);
+    f.setJMenuBar(menuBar);
+    f.add(g);
+    f.pack();
+    f.setVisible(true);
+    f.setTitle("Snake");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setResizable(false);
+    f.setLocationRelativeTo(null);
+
 }
